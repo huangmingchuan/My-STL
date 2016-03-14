b790749527831aeaa4d890fbe5592f926aeae590
huangmingchuan <hmcxxdxx@gmail.com>
2016-03-15 03:00:01 +0800

    construct.h

--- stl/src/construct.h
+++ stl/src/construct.h
@@ -6,7 +6,36 @@
 
 namespace mystl
 {
+	template<typename T1, typename T2>
+	inline void construct(T1 *ptr1, const T2& value)
+	{
+		new (ptr1)T1(value);
+	}
 
+	template<typename T>
+	inline void destroy(T *ptr)
+	{
+		ptr->~T();
+	}
+
+	template<typename ForwardIterator>
+	inline void _destroy(ForwardIterator first, ForwardIterator last, std::true_type) {}
+
+	template<typename ForwardIterator>
+	inline void _destroy(ForwardIterator first, ForwardIterator last, std::false_type)
+	{
+		for (; first != last; ++first)
+		{
+			destroy(&*first);
+		}
+	}
+
+	template<typename ForwardIterator>
+	inline void destroy(ForwardIterator first, ForwardIterator last)
+	{
+		typedef typename std::is_pod<ForwardIterator> pod_type;
+		_destroy(first, last, pod_type);
+	}
 }
 
 #endif
