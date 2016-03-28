07fb58667ba7d9d1f0ff09234a91542a1adce7c7
huangmingchuan <hmcxxdxx@gmail.com>
2016-03-28 23:21:25 +0800

    添加操作内存的工具函数

---
+++ stl/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "src\allocator.h"
+#include "src\construct.h"
+#include "src\uninitialized_functions.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "hello world" << endl;
+	return 0;
+}
