c9cc0435d13e44751aa3c0510eb0b15c19d43f23
huangmingchuan <hmcxxdxx@gmail.com>
2016-04-22 22:52:12 +0800

    add string.h

---
+++ stl/src/string.h
@@ -0,0 +1,20 @@
+#ifndef _STRING_H_
+#define _STRING_H_
+
+#include "allocator.h"
+#include "reverse_iterator.h"
+#include "uninitialized_functions.h"
+
+#include <cstring>
+#include <type_traits>
+
+namespace mystl
+{
+	class string
+	{
+
+	};
+}
+
+
+#endif
